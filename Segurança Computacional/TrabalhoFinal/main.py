02ef42d957f1fb998d151ebcdb9cfacdc9d85e25
Gabriel ARP <garpereira@unifesp.br>
2023-05-17 15:20:11 -0300

    Adicionado as mudancas de cor das mensagens de acordo com a identidade

--- Segurança Computacional/TrabalhoFinal/main.py
+++ Segurança Computacional/TrabalhoFinal/main.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5 import *
 
 class mainWindow(QMainWindow):
+
+    __instancia__: None
+
     def __init__(self):
         super().__init__()
         screen_geo = QDesktopWidget().availableGeometry()
@@ -24,14 +28,18 @@ class mainWindow(QMainWindow):
         self.text_KEY.setPlaceholderText('Digite a Chave Cripto')
         self.text_KEY.setGeometry(10, 70, 280, 30)
 
-        self.checkbox_RC4 = QCheckBox('RC4', self)
-        self.checkbox_RC4.setGeometry(320, 50, 100, 30)
+        self.opt_ALGORITHMS = QComboBox(self)
+        self.opt_ALGORITHMS.addItems(['SDES', 'RC4'])
+        self.opt_ALGORITHMS.setGeometry(290, 70, 100, 30)
 
-        self.checkbox_SDES = QCheckBox('SDES', self)
-        self.checkbox_SDES.setGeometry(320, 80, 100, 30)
+        # self.checkbox_RC4 = QCheckBox('RC4', self)
+        # self.checkbox_RC4.setGeometry(320, 50, 100, 30)
+
+        # self.checkbox_SDES = QCheckBox('SDES', self)
+        # self.checkbox_SDES.setGeometry(320, 80, 100, 30)
 
         self.text_MSGCHAT = QTextEdit(self)
-        self.text_MSGCHAT.setGeometry(10, 200, 380, 300)
+        self.text_MSGCHAT.setGeometry(10, 110, 380, 400)
 
         scrollbar = QScrollBar(self)
         scrollbar.setGeometry(380, 200, 20, 300)
@@ -44,20 +52,40 @@ class mainWindow(QMainWindow):
 
         self.btn_Enviar = QPushButton('Enviar', self)
         self.btn_Enviar.setGeometry(290, 540, 100, 30)
-        self.btn_Enviar.clicked.connect(lambda: self.enviar_mensagem())
+        self.btn_Enviar.clicked.connect(lambda: self.enviar_mensagem('servidor'))
 
     def eventFilter(self, obj, event):
         if event.type() == QEvent.KeyPress and obj is self.text_MSG:
             if event.key() == Qt.Key_Return:
-                self.enviar_mensagem()
+                self.enviar_mensagem('servidor')
                 return True
             return False
         return False
+    
+    def conectar(self):
+        pass
 
-    def enviar_mensagem(self):
+    def enviar_mensagem(self, identidade):
         mensagem = self.text_MSG.toPlainText()
+
+        if identidade == 'servidor':
+            cor = QColor(255, 0 ,0)
+
+        elif identidade == 'cliente':
+            cor = QColor(0, 0, 255)
+
+        formato = QTextCharFormat()
+        formato.setForeground(cor)
+        formato.setFontWeight(QFont.Bold)
+
+        self.text_MSGCHAT.setCurrentCharFormat(formato)
         self.text_MSGCHAT.append(mensagem)
         self.text_MSG.clear()
+
+    def get_instancia(cls):
+        if not cls.__instancia__:
+            cls.__instancia__= super().get_instancia(cls)
+        return cls.__instancia__
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
